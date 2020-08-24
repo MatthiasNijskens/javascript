73d99dec498aedcbad5a61b33bb82542c80a9965
Matthias <matthias.nijskens@outlook.com>
2020-08-24 11:54:43 +0200

    finished second exercise

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -9,8 +9,10 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
+    var name = prompt("What is your name?");
+    alert("Hello," + name);
 
     // your code here
 
-})();
+
