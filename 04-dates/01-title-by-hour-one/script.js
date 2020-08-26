b79ad96909a32af3d79b1bee9a187dd279c050ce
Matthias <matthias.nijskens@outlook.com>
2020-08-26 14:37:05 +0200

    finished exercise 1

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -14,5 +14,12 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let day = new Date();
+    let time = day.getHours();
+    if (time < 18) {
+        document.getElementById("target").innerHTML = "Hello";
+    } else {
+        document.getElementById("target").innerHTML = "Good evening";
+    }
 
 })();
