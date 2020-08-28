34114bb75961c2e2cde6381d24b3a17400859cdf
Matthias <matthias.nijskens@outlook.com>
2020-08-28 12:02:35 +0200

    finished exercise 9 generated array length 10 with random numbers

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,6 +11,21 @@
 
 (function() {
 
-    // your code here
-
+    document.querySelector("button").onclick = function (event) {
+        let numbers = [];
+        for (x = 0; x < 10; x++){
+            numbers.push(Math.floor(Math.random() * Math.floor(100)));
+        }
+        for (i = 1; i <=10; i++) {
+            document.getElementById("n-"+i).innerHTML = numbers[i-1];
+        }
+        let min = Math.min(...numbers);
+        let max = Math.max(...numbers);
+        let sum = numbers.reduce((a, b) => a + b, 0);
+        let avg = sum/10;
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("max").innerHTML = max;
+        document.getElementById("sum").innerHTML = sum;
+        document.getElementById("average").innerHTML = avg;
+    }
 })();
