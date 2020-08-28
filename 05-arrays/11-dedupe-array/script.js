e80659d36fdf5dea9bde5fd31d916434d1c03ad2
Matthias <matthias.nijskens@outlook.com>
2020-08-28 15:12:05 +0200

    finished exercise 11 with new Set data-structure

--- 05-arrays/11-dedupe-array/script.js
+++ 05-arrays/11-dedupe-array/script.js
@@ -28,6 +28,9 @@
         "cerise",
     ];
 
-    // your code here
+    document.querySelector("button").onclick = function (event){
+        let unique = [...new Set(fruits)];
+        console.log(unique);
+    }
 
 })();
